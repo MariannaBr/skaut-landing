d8137a25d5550704efda194efc54621dcf935364
Marianna Brezinova <brezinova.marianna@gmail.com>
2025-01-09 14:56:35 -0800

    fixed fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 import { getDataFromFirestore } from "../../lib/firestore";
 import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await getDataFromFirestore();
-  return {
-    props: { data }
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const data = await getDataFromFirestore();
+//   return {
+//     props: { data }
+//   };
+// };
 
-export default function Home() {
+export default async function Home() {
+  const data = await getDataFromFirestore();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
