3efb36f3788a3b5d28d99c1f44458102cd320e5d
Marianna Brezinova <brezinova.marianna@gmail.com>
2025-01-09 14:33:11 -0800

    added firestore db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import { getDataFromFirestore } from "../../lib/firestore";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const data = await getDataFromFirestore();
+  return {
+    props: { data }
+  };
+}
+
+export default function Home({ data }) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -96,6 +104,12 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <h1>Firestore Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
