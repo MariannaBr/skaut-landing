365d325620bdc18179b3038f61f3cbb796af40f4
Marianna Brezinova <brezinova.marianna@gmail.com>
2025-04-21 15:09:43 -0700

    commented calendar for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,20 +41,12 @@ const Homepage: React.FC = () => {
             {formTitleTime}
           </p> */}
         </div>
+
         {/* Calendly inline widget begin */}
-        <InlineWidget url="https://calendly.com/brezinova-marianna/test-meeting" />
-        {/* <div
-          class="calendly-inline-widget"
-          data-url="https://calendly.com/brezinova-marianna/test-meeting"
-          style="min-width:320px;height:700px;"
-        ></div>
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async
-        ></script> */}
+        {/* <InlineWidget url="https://calendly.com/brezinova-marianna/test-meeting" /> */}
 
         {/*Calendly inline widget end */}
+
         {/* <div className="flex mx-4 xl:mx-auto justify-center max-w-7xl px-4 py-10 shadow-2xl shadow-teal-200 rounded-2xl">
           <Form survey={survey} />
         </div> */}
