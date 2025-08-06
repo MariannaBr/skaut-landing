9aa546a1d2a414764b1d524fe34bc39e5181486f
Marianna <brezinova.marianna@gmail.com>
2025-08-05 21:01:55 -0700

    added profile

--- components/Form.tsx
+++ components/Form.tsx
@@ -43,66 +43,62 @@ export default function Form() {
   return (
     <>
       <form onSubmit={handleSubmit} className="h-full flex flex-col">
-        <div className="flex-1 space-y-6">
-          <div className="space-y-6">
-            <div className="space-y-4">
-              <fieldset>
-                <div className="space-y-6">
-                  {survey.answers.map((answer, index) => (
-                    <div key={index} className="flex items-center gap-x-3">
-                      <input
-                        id={`${index}`}
-                        name="response"
-                        type="radio"
-                        value={answer}
-                        checked={formData.response === answer}
-                        onChange={() => handleRadioChange(answer)}
-                        className="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white checked:border-pink-600 checked:bg-pink-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden [&:not(:checked)]:before:hidden"
-                      />
-                      <label
-                        //htmlFor={`${sectionIndex}-${question.id}-${index}`}
-                        className="block text-sm/6 font-medium text-gray-900"
-                      >
-                        {answer}
-                      </label>
-                    </div>
-                  ))}
-                  
-                  {/* Conditional text input for "Other" option */}
-                  {formData.response === "Other" && (
-                    <div className="ml-7 mt-4">
-                      <label
-                        htmlFor="otherResponse"
-                        className="block text-sm/6 font-medium text-gray-900 mb-2"
-                      >
-                        Please specify:
-                      </label>
-                      <input
-                        id="otherResponse"
-                        name="otherResponse"
-                        type="text"
-                        value={formData.otherResponse || ""}
-                        onChange={handleChange}
-                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-pink-500 sm:text-sm/6"
-                        placeholder="Enter your response..."
-                      />
-                    </div>
-                  )}
+        <div className="flex-1">
+          <fieldset className="my-10">
+            <div className="space-y-6">
+              {survey.answers.map((answer, index) => (
+                <div key={index} className="flex items-center gap-x-3">
+                  <input
+                    id={`${index}`}
+                    name="response"
+                    type="radio"
+                    value={answer}
+                    checked={formData.response === answer}
+                    onChange={() => handleRadioChange(answer)}
+                    className="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white checked:border-pink-600 checked:bg-pink-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden [&:not(:checked)]:before:hidden"
+                  />
+                  <label
+                    //htmlFor={`${sectionIndex}-${question.id}-${index}`}
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    {answer}
+                  </label>
                 </div>
-              </fieldset>
+              ))}
 
-              <div className="sm:col-span-4">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  placeholder="Your email address"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-pink-500 sm:text-sm/6"
-                />
-              </div>
+              {/* Conditional text input for "Other" option */}
+              {formData.response === "Other" && (
+                <div className="ml-7 mt-4">
+                  <label
+                    htmlFor="otherResponse"
+                    className="block text-sm/6 font-medium text-gray-900 mb-2"
+                  >
+                    Please specify:
+                  </label>
+                  <input
+                    id="otherResponse"
+                    name="otherResponse"
+                    type="text"
+                    value={formData.otherResponse || ""}
+                    onChange={handleChange}
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-pink-500 sm:text-sm/6"
+                    placeholder="Enter your response..."
+                  />
+                </div>
+              )}
             </div>
+          </fieldset>
+
+          <div className="sm:col-span-4">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Your email address"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-pink-500 sm:text-sm/6"
+            />
           </div>
 
           <div className="mt-auto flex items-center justify-end gap-x-6 pt-6">
--- pages/index.tsx
+++ pages/index.tsx
@@ -38,27 +38,45 @@ const Homepage: React.FC = () => {
               Everything you need to deploy your app
             </p>
             <div className="mt-6 grid gap-3 sm:mt-8 lg:grid-cols-3 lg:grid-rows-2">
+              {/* Marianna */}
               <div className="relative lg:row-span-2">
-                <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
-                <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
-                  <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-                    <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                      Mobile friendly
-                    </p>
-                    <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                      Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                      irure qui lorem cupidatat commodo.
-                    </p>
-                  </div>
-                  <div className="relative min-h-[20rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
-                    <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
-                      <img
-                        alt=""
-                        src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
-                        className="size-full object-cover object-top"
-                      />
-                    </div>
-                  </div>
+                <img
+                  alt="Marianna Brezinova"
+                  src="/Marianna.jpg"
+                  className="mx-auto size-56 rounded-full outline outline-1 -outline-offset-1 outline-black/5 mt-8"
+                  //className="aspect-[3/2] w-full rounded-t-2xl object-contain object-center bg-black outline outline-1 -outline-offset-1 outline-black/5"
+                />
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <h3 className="text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                    Marianna Brezinova
+                  </h3>
+                  <p className="text-base/7 text-gray-600">Founder</p>
+                  <p className="mt-4 text-base/7 text-gray-600">
+                    Marianna is a founder of Skaut.ai and I'm here to help you
+                    with automation.
+                  </p>
+                  <ul role="list" className="mt-6 flex gap-x-6">
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/mariannabrezinova/"
+                        className="text-gray-400 hover:text-gray-500"
+                      >
+                        <span className="sr-only">LinkedIn</span>
+                        <svg
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          aria-hidden="true"
+                          className="size-5"
+                        >
+                          <path
+                            d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                            clipRule="evenodd"
+                            fillRule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
                 <div className="pointer-events-none absolute inset-px rounded-lg shadow outline outline-black/5 lg:rounded-l-[2rem]" />
               </div>
@@ -99,7 +117,7 @@ const Homepage: React.FC = () => {
                   <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
                     <img
                       alt=""
-                      src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-security.png"
+                      src="/apps_graph.png"
                       className="h-[min(152px,40cqw)] object-cover"
                     />
                   </div>
